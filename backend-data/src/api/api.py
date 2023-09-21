41d567ee25ae9e6b4a415c101768f06bf46dd0d3
Guillermo Murcia <guiller.512@gmail.com>
2023-09-21 12:27:24 +0200

    terminar endpoint get_todays_info

--- backend-data/src/api/api.py
+++ backend-data/src/api/api.py
@@ -1,42 +1,53 @@
-from flask import Flask, jsonify, request, Response
+from flask import Flask, request, Response
 import os
 import sqlite3
+import datetime
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 app = Flask(__name__)
-# app.config["DEBUG"] = True
 
 
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>My API</h1><p>This site is an API for costal water quality info.</p>"
 
-# 1.Ruta para obtener los datos de todas las playas
-@app.route('/v0/get_todays_info', methods=['GET'])
+# 1.ruta para obtener los datos de hoy de todas las playas
+@app.route('/get_todays_info', methods=['GET'])
 def get_todays_info():
+
+    # coger datos de hoy de la DB
     conn = sqlite3.connect('../db/DB.db')
     crsr = conn.cursor()
 
+    date_today = datetime.date.today().strftime("%d/%m/%Y")
+    sql = '''
+        SELECT * FROM beach_water_info WHERE date = ?
+    '''
+    crsr.execute(sql, (date_today,))
+    ans = crsr.fetchall()
+
     crsr.close()
     conn.close()
-    info = {}
-#   {
-#       "beaches": [
-#       {
-#         "name": "Playa de la Malagueta",
-#         "info": {
-#             "e-coli": 20,
-#             "enterococcus": 10,
-#             "ph": 1,
-#             "chemical-1": 1,
-#             "quality": 1
-#         }
-#       },
-#       {
-#           'name':
-#       }
-#     ]
-#   }
-    return info
+
+    # formatear los datos para 
+    res = {'date': date_today}
+    beaches = []
+
+    for row in ans:
+        date, name, e_coli, enterococcus, ph, ammonium, mercury, turbidity, quality = row
+        beaches.append({
+            'name': name,
+            'info':{
+                'e-coli': e_coli,
+                'enterococcus': enterococcus,
+                'ph': ph,
+                'ammonium': ammonium,
+                'mercury':mercury,
+                'turbidity': turbidity,
+                'quality': quality
+            }
+        })
+    res['beaches'] = beaches
+    return res
 
 app.run(host='0.0.0.0')
