d578fa9912913fd08ec32626da0a7712a6652c1d
DPachoGit <davidpacho1513@gmail.com>
2023-09-21 20:48:37 +0200

    Connection with backend done. It remains to be verified

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
-import './App.css'
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
-import Router from'./routes/router';
+import Router from './routes/router';
+import { BeachDataProvider } from './context/beachDataContext';
 
 function App() {
   return (
-    <>
-    <RouterProvider router={Router} />
-    </>
-  )
+    <BeachDataProvider>
+      <RouterProvider router={Router} />
+    </BeachDataProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/addBeach.jsx
@@ -0,0 +1,27 @@
+import { useBeachData } from '../context/beachDataContext';
+
+const AddBeach = async (email, beach) => {
+  const { addBeachToFavorites } = useBeachData();
+
+  try {
+    const result = await fetch('http://localhost:3333/api/addbeach', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email, beach })
+    });
+
+    if (result.ok) {
+      const data = await result.json();
+      addBeachToFavorites(data.favBeaches);
+    } else {
+
+      console.error('Error adding beach to favorites');
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export default AddBeach;
